1d908f58890807bfedd37bb6f5a72885ae97aa13
Sum1likegod <it2037@global.org.in>
2023-12-29 10:56:00 +0530

    OOPS

--- constructrs.py
+++ constructrs.py
@@ -1,13 +1,18 @@
-class Defa_con():
-  def __init__(self):
-    print("This is Default Constructor as it has only one argument")
+class Defa_con:
+    def __init__(self):
+        print("This is Default Constructor as it has only one argument")
+
 
 Defa_con()
 
-class Para_con():
-  def __init__(self,ar_1,ar_2):
-    self.a = ar_1
-    self.b = ar_2
-    print(f"This is Parameterized Constructor as it has two arguments {ar_1} and {ar_2}")
 
-Para_con(1,2)
+class Para_con:
+    def __init__(self, ar_1, ar_2):
+        self.a = ar_1
+        self.b = ar_2
+        print(f"This is Parameterized Constructor as it has two arguments {ar_1} and {ar_2}")
+
+
+obj = Para_con(1, 2)
+print(f"{obj.a} is the argument of the object obj for the class Para_con")
+print(obj.b)
