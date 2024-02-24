fa2d37eb4a5f82a65139f68fed802d956b4f6532
Sum1likegod <it2037@global.org.in>
2024-02-24 17:52:38 +0530

    Revising the Mistakes

--- Dict_methods.py
+++ Dict_methods.py
@@ -1,17 +1,19 @@
-dict_ex = {"item_1":'nope', "item_2":'hope', "item_3":'pope'}
+# dict_ex = {"item_1":'nope', "item_2":'hope', "item_3":'pope'}
 
-print(dict_ex.items())
-print(dict_ex.values())
-print(dict_ex.keys())
-
-
-
-dict_ex_2 = {"item_5":'rope', "item_4":'dope', "item_3":'change' }
-
-dict_ex.update(dict_ex_2)
-print('\n',dict_ex)
-print(dict_ex_2)
-dict_ex_2.clear()
-print(dict_ex_2)
+# print(dict_ex.items())
+# print(dict_ex.values())
+# print(dict_ex.keys())
 
 
+# dict_ex_2 = {"item_5":'rope', "item_4":'dope', "item_3":'change' }
+
+# dict_ex.update(dict_ex_2)
+# print('\n',dict_ex)
+# print(dict_ex_2)
+# dict_ex_2.clear()
+# print(dict_ex_2)
+
+# print(dict_ex.pop("item_1"))
+# print(dict_ex)
+# dict_ex.popitem()
+# print(dict_ex)
--- Snake_water_game.py
+++ Snake_water_game.py
@@ -1,19 +1,19 @@
-import random 
+import random
 
 while True:
 
-  sn_wa_gu = [0,1,2]
-  comp = random.choice(sn_wa_gu)
-  user = int(input("Enter 0 for snake, 1 for water and 2 for gun: "))
-  if user == 5:
-    break
-  if not -1 < int(user) < 3:
-    raise ValueError("Invalid input")
-  print(f'\t\tComputer\'s Input is {comp}')
-  print(f'\t\tUser\'s Input is {user}')
-  if comp == user:
-    print("The Game Has Been Drawed")
-  elif (comp == 0 and user == 2) or (comp == 1 and user == 0) or (comp == 2 and user == 1):
-    print("User Won The Game")
-  elif (comp == 0 and user == 1) or (comp == 1 and user == 2) or (comp == 2 and user == 0):
-    print("User Lose The Game")
+    sn_wa_gu = [0, 1, 2]
+    comp = random.choice(sn_wa_gu)      #random.randint(starting_index , end_index) which return random integer in between them
+    user = int(input("Enter 0 for snake, 1 for water and 2 for gun: "))
+    if user == 5:
+        break
+    if not -1 < int(user) < 3:
+        raise ValueError("Invalid input")
+    print(f'\t\tComputer\'s Input is {comp}')
+    print(f'\t\tUser\'s Input is {user}')
+    if comp == user:
+        print("The Game Has Been Drew")
+    elif (comp == 0 and user == 2) or (comp == 1 and user == 0) or (comp == 2 and user == 1):
+        print("User Won The Game")
+    elif (comp == 0 and user == 1) or (comp == 1 and user == 2) or (comp == 2 and user == 0):
+        print("User Lose The Game")
